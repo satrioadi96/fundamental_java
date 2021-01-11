b838bd797d85095eb61cbd6906f222b426488b84
radi2 <radi2@DESKTOP-IGJC24N>
2021-01-12 03:02:36 +0700

    Upload for new file class of Java

---
+++ fundamental00/src/dasar/variabel/KelasVariabel.java
@@ -0,0 +1,48 @@
+package dasar.variabel;
+
+public class KelasVariabel {
+	public static void main(String[] args) {
+		// TODO auto-geenrated method stub
+		
+		// int
+		// double
+		// Long
+		// float
+		// Boolean
+		// String
+		// char
+		
+		int angka0 = 11; // < Long
+		
+		Integer angkaNull = null;
+		
+		Double angkaKoma = 12.6; // > float
+		
+		double angkaKoma00 = 0.22;
+		
+		Long angkaL = 1007000464354352200L; // < int
+		
+		long angka3 =2012222223;
+		
+		Float angkaKoma2 = 30.1081f; // < double
+		
+		float angkakoma0 = -0.94846475949245f;
+		
+		Boolean inti = true;
+		
+		String kata = "Siapa saja !";
+		
+		char res = 'h';
+		
+		System.out.println( angka0 );System.out.println( angkaNull );
+		System.out.println( angkaKoma );System.out.println( angkaKoma00 );
+		System.out.println( angkaL );System.out.println( angkaKoma2 );
+		System.out.println( angkakoma0 );System.out.println( inti );
+		System.out.println( kata );System.out.println( res );
+		System.out.println( angka3 );
+		
+		System.out.println( "int "+angka0+" + "+" Double "+angkaKoma+" "+" beda dengan "+angka3 );
+		
+	}
+
+}
